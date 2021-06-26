94ebb85e28ecaad23206922fe2503e19e503729b
extremeheat <extreme@protonmail.ch>
2021-06-26 01:19:16 -0400

    py: remove proxy duplicate function wrapper

--- src/JSPyBridge/proxy.py
+++ src/JSPyBridge/proxy.py
@@ -85,61 +85,21 @@ class Executor:
         return self.loop.remove_listener(what, event, handler)
 
 
-INTERNAL_VARS = ["ffid", "_ix", "_exe", "_iffid"]
+INTERNAL_VARS = ["ffid", "_ix", "_exe", "_pffid", "_pname"]
 
 # "Proxy" classes get individually instanciated for every thread and JS object
 # that exists. It interacts with an Executor to communicate.
 class Proxy(object):
-    def __init__(self, exe, ffid, iffid=0):
+    def __init__(self, exe, ffid, prop_ffid=None, prop_name=''):
         self.ffid = ffid
         self._exe = exe
         self._ix = 0
-        self._iffid = iffid
+        # 
+        self._pffid = prop_ffid if (prop_ffid != None) else ffid
+        self._pname = prop_name
 
     def _call(self, method, methodType, val):
         this = self
-        # TODO: Remove this and just return Proxy to avoid duplicating code
-        class fn:
-            def __init__(self, ffid):
-                self.ffid = ffid
-
-            def __str__(self):
-                return this._exe.inspect(self.ffid)
-
-            def __repr__(self):
-                return this._exe.inspect(self.ffid)
-
-            def __call__(self, *args):
-                mT, v = this._exe.callProp(this.ffid, method, args)
-                # bleh, functions inside functions cause inf recursion
-                # can we avoid from JS? --done, with { call } wrapper
-                if mT == "fn":
-                    # print("NESTED", method, methodType, mT, v)
-                    return Proxy(this._exe, v)
-                    # raise Error("Generator functions are not supported right now")
-                # print("__call", mT, v)
-                return this._call(method, mT, v)
-
-            def __getattr__(self, attr):
-                # print("CALLED getattr", attr)
-                if attr == "new":
-                    # mT, v = this._exe.initProp(this.ffid, method)
-                    return this._call("", "class", this.ffid)
-                raise Exception(
-                    "Cannot access variable inside a function type, did you forget to use .new()?"
-                )
-                # methodType, val = this._exe.getProp(this.ffid, attr)
-                # return this._call(attr, methodType, val)
-
-            # TODO: Free !!!
-
-            def __json__(self):
-                # important ref
-                return {"ffid": self.ffid}
-
-            def __del__(self):
-                this._exe.free(self.ffid)
-
         def instantiatable(*args):
             mT, v = self._exe.initProp(self.ffid, method, args)
             # when we call "new" keyword we always get object back
@@ -148,33 +108,33 @@ class Proxy(object):
         debug("MT", method, methodType, val)
         if methodType == "fn":
             # print("ret fn", method)
-            return fn(val)
+            return Proxy(self._exe, val, self.ffid, method)
         if methodType == "class":
             # print("ret cls", method)
             return instantiatable
         if methodType == "obj":
             return Proxy(self._exe, val)
         if methodType == "inst":
-            return Proxy(self._exe, val, self.ffid)
+            return Proxy(self._exe, val)
         if methodType == "void":
             return None
         else:
             return val
 
     def __call__(self, *args):
-        mT, v = self._exe.callProp(self.ffid, "", args)
+        mT, v = self._exe.callProp(self._pffid, self._pname, args)
         # bleh, functions inside functions cause inf recursion
         # can we avoid from JS? --done, with { call } wrapper
         if mT == "fn":
-            raise Error("Generator functions are not supported right now")
+            return Proxy(self._exe, v)
         # print('Callres', self.ffid, args, mT, v)
-        return self._call("", mT, v)
+        return self._call(self._pname, mT, v)
 
     def __getattr__(self, attr):
         # Special handling for new keyword for ES5 classes
         if attr == "new":
-            return self._call(attr, "class", self.ffid)
-        methodType, val = self._exe.getProp(self.ffid, attr)
+            return self._call(attr if self._pffid == self.ffid else '', "class", self._pffid)
+        methodType, val = self._exe.getProp(self._pffid, attr)
         return self._call(attr, methodType, val)
 
     def __getitem__(self, attr):
