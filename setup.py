258f1180f9c342a4841c2cf5ca14c86dd741650c
extremeheat <extreme@protonmail.ch>
2021-07-17 14:01:22 -0400

    py: Fixes for Google Colab and Jupyter notebooks (#15)
    * py: downgrade to node 14, python 3.7
    
    * py: fix colab, notebook imports

--- setup.py
+++ setup.py
@@ -96,6 +96,7 @@ setup(
         # that you indicate you support Python 3. These classifiers are *not*
         # checked by 'pip install'. See instead 'python_requires' below.
         'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: 3 :: Only',
@@ -128,7 +129,7 @@ setup(
     # 'Programming Language' classifiers above, 'pip install' will check this
     # and refuse to install the project if the version does not match. See
     # https://packaging.python.org/guides/distributing-packages-using-setuptools/#python-requires
-    python_requires='>=3.8, <4',
+    python_requires='>=3.7, <4',
 
     # This field lists other packages that your project depends on to run.
     # Any package you put here will be installed by pip when your project is
