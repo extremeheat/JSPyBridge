86701261c8efff981cb7b9e3ba82d7398ced8f72
Romain Beaumont <romain.rom1@gmail.com>
2021-11-24 08:45:59 +0100

    Update version to 14 in index.js

--- src/pythonia/index.js
+++ src/pythonia/index.js
@@ -1,4 +1,4 @@
-if (typeof process !== 'undefined' && parseInt(process.versions.node.split('.')[0]) < 16) {
+if (typeof process !== 'undefined' && parseInt(process.versions.node.split('.')[0]) < 14) {
   console.error('Your node version is currently', process.versions.node)
   console.error('Please update it to a version >= 16.x.x from https://nodejs.org/')
   process.exit(1)
