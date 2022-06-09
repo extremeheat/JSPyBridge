7879bdaf32d214a9b48f27272f9a1c767d0860f9
Marcus <marcusds@users.noreply.github.com>
2022-06-09 12:46:36 -0700

    fix: type definitions so can build under strict (#56)

--- src/pythonia/index.d.ts
+++ src/pythonia/index.d.ts
@@ -1,22 +1,22 @@
-export function python(fileName: string): Promise
-// Template function to evaulate Python code 
-export function py(tokens, ...replacements)
+export function python(fileName: string): Promise<any>
+// Template function to evaulate Python code
+export function py(tokens: any[], ...replacements: any[]): any
 
 interface py {
   // Template function to exec() Python code
-  exec(tokens, ...replacements): Promise<any>
+  exec(tokens: any[], ...replacements: any[]): Promise<any>
 
   /**
    * Creates a Python Set, `set(1, 2, 3)`. Most APIs accept normal arrays, but some require set/tuple types.
    * @param items Items to include in this set
    */
-  set(...items): Promise<any>
-  
+  set(...items: any[]): Promise<any>
+
   /**
    * Creates a Python tuple, `tuple(1, 2, 3)`. Most APIs accept normal arrays, but some require set/tuple types.
    * @param items Items to include in this tuple
    */
-  tuple(...items): Promise<any>
+  tuple(...items: any[]): Promise<any>
 
   /**
    * Same as Python's enumerate(). Used for for loops. For example,
@@ -27,7 +27,7 @@ interface py {
    * ```
    * @param item The Python object
    */
-  enumerate(item): Promise<any>
+  enumerate(item: python): Promise<any>
 
   /**
    * The Python with statement, the first parameter is the Python object
@@ -38,15 +38,15 @@ interface py {
    * })
    * ```
    */
-  with(statement, callback: (handle: any) => Promise<void>): Promise<void>
+  with(statement: python, callback: (handle: any) => Promise<void>): Promise<void>
 }
 
 interface python {
   /**
    * This toggles "Fast Mode", where the bridge skips string serialization. With this enabled, when using console.log
-   * you now need to await object.toString(). For example, 
+   * you now need to await object.toString(). For example,
    * ```js
-   * const res = await somePythonApi(); 
+   * const res = await somePythonApi();
    * console.log(await res.toString())
    * ```
    * @param value True or false
@@ -58,7 +58,7 @@ interface python {
    * Change the Python process working dir. Allows for file system loads relative to the working dir.
    * @param path The path to set the work dir to. If blank, pick the current file's folder path.
    */
-  cwd(path): void
+  cwd(path: string): void
 
   /**
    * Quits the Python process. You can also do `process.exit()` if you want to kill the current process, including Python.
@@ -70,15 +70,15 @@ export class PyClass {
   /**
    * Creates a Python class. **You must use** `await MyClass.init()` when initializing, don't just do `new MyClass()`.
    * The JS  constructor is called *before* the Python class has been `__init__`'ed. Your `init()` method is called
-   * after the Python class has been constructed. 
-   * @param superclass The Python superclass you want to extend, optional. 
+   * after the Python class has been constructed.
+   * @param superclass The Python superclass you want to extend, optional.
    * @param superArguments The positional arguments you want to pass to the super `__init__`
    * @param superKeywordArguments The keyword arguments you want to pass to the super `__init__`
    */
-  constructor(superclass, superArguments: [], superKeywordArguments: {})
+  constructor(superclass: python, superArguments: [], superKeywordArguments: {})
 
   /**
    * This class is called after the Python class has been created.
    */
-  init()
+  init(): any
 }
